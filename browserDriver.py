fd4c4d68f2058de60f46d83dcbbae6144808a780
desa-thomas <desa2thomas@gmail.com>
2024-07-19 00:11:12 -0400

    driver waits for page to load before grabbing src
    created waits() loop to wait for page to load by
    comparing the grabbed <video> tag src property against
    the tik tok loading animation.

--- browserDriver.py
+++ browserDriver.py
@@ -1,10 +1,9 @@
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
-    
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
+from selenium.webdriver.support.wait import WebDriverWait
+from functions import *
 
-driver.get("")
+import time
 
 #TO DO
 """
@@ -24,5 +23,42 @@ driver.get("")
 
 """
 
+
+"""
+    Checks if the video found on the page is the tik tok loading video, if so it returns false, otherwise true. 
+    This function is passed into "wait.until()", the until function calls this on a loop until the function returns true, 
+    effectivly waiting until the page loads. 
+
+    parameter: 
+        driver - WebDriver passed into the lambda function (webdriver)
+
+    returns: 
+        boolean - true if the video is not the tik tok loading animation. 
+"""
+def checkSrc(driver):
+    src = driver.find_element(By.TAG_NAME, "video").get_property("src")
+    print("waiting...")
+    return src != "https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/webapp-desktop/playback1.mp4"
+
+#create webdriver
+driver = webdriver.Chrome()
+driver.implicitly_wait(40)
+
+driver.get("https://www.tiktok.com/en")
+
+# wait until the page loads to retrieve video
+try: 
+    WebDriverWait(driver, 15).until(lambda driver: checkSrc(driver))
+    print("video found")
+
+except TimeoutError: 
+    print("timed out")
+
+#download video
+src = driver.find_element(By.TAG_NAME, "video").get_property("src")
+print(src)
+driver.get(src)
+
+time.sleep(10)
 
 driver.quit()
