f19a19e65f295b17ab7d2509b77c51d90b1a8e0b
desa-thomas <desa2thomas@gmail.com>
2024-07-29 23:51:04 -0400

    created scrape_video function - also added function documentation

--- browserDriver.py
+++ browserDriver.py
@@ -3,59 +3,80 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 import requests
+import time
+import pathlib
 
-directory = "C:/Users/desa2/Desktop/ProjectsDigital Marketing Script"
+def checkSrc(driver: webdriver.Chrome) -> bool:
+    """
+    Checks if the video found on the page is not the tik tok loading video. 
 
-"""
-    Checks if the video found on the page is the tik tok loading video, if so it returns false, otherwise true. 
-    This function is passed into "wait.until()", the until function calls this on a loop until the function returns true, 
-    effectivly waiting until the page loads. 
+    ### Parameters
+        driver : webdriver.Chrome
+            - WebDriver passed into the lambda function (webdriver)
 
-    parameter: 
-        driver - WebDriver passed into the lambda function (webdriver)
+    ### Returns
+        boolean : bool
+            - true if the video is not the tik tok loading animation. 
+    """ 
 
-    returns: 
-        boolean - true if the video is not the tik tok loading animation. 
-"""
-def checkSrc(driver):
     src = driver.find_element(By.TAG_NAME, "video").get_property("src")
     print("waiting...")
     
     # Returns true when the video is not the loading animation
     return src != "https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/webapp-desktop/playback1.mp4"
 
-#Script starts ---------------------------------------------------------------------------------------------
+
+def scrape_video(driver: webdriver.Chrome): 
+    """
+    Scrapes first Tiktok video from user's for you page (must be logged in on driver). Saves video to "video.mp4"
+
+    ### parameters
+        driver : webdriver.Chrome
+            - chrome webdriver doing the scraper
+    ### returns
+        nothing
+    """
+    driver.get("https://www.tiktok.com/en")
+
+    # wait until the page loads to retrieve video
+    try: 
+        WebDriverWait(driver, 15).until(lambda driver: checkSrc(driver))
+        print("video found")
+
+    except TimeoutError: 
+        print("timed out")
+
+
+    src = driver.find_element(By.TAG_NAME, "video").get_property("src")
+
+    #Copy the selenium sessions cookies to a request session to download the mp4 file. 
+    cookies = driver.get_cookies()
+    session = requests.Session()
+    for cookie in cookies: 
+        session.cookies.set(cookie['name'], cookie['value'])
+
+    file_bytes = session.get(src).content
+    with open("video.mp4", "wb") as f:
+        f.write(file_bytes)
+        print("saved video")
+        f.close()
+    
+    return 
+
+
+#---------------------------------------------------------------------------------------
+directory = pathlib.Path().absolute()
 
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")
+# chrome_options.add_argument("--headless=new")
+chrome_options.add_argument(f"--user-data-dir={directory}\\userdata")
 
 #create webdriver
 driver = webdriver.Chrome(options = chrome_options)
 
-driver.get("https://www.tiktok.com/en")
+scrape_video(driver)
 
-# wait until the page loads to retrieve video
-try: 
-    WebDriverWait(driver, 15).until(lambda driver: checkSrc(driver))
-    print("video found")
-
-except TimeoutError: 
-    print("timed out")
-
-
-src = driver.find_element(By.TAG_NAME, "video").get_property("src")
-
-#Copy the selenium sessions cookies to a request session to download the mp4 file. 
-cookies = driver.get_cookies()
-session = requests.Session()
-for cookie in cookies: 
-    session.cookies.set(cookie['name'], cookie['value'])
-
-file_bytes = session.get(src).content
-with open("video.mp4", "wb") as f:
-    f.write(file_bytes)
-    print("saved video")
-
+time.sleep(1000)
 driver.quit()
 
 """
