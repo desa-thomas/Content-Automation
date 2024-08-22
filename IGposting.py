9388116cd4d356987ad895a19e0ffa4bf807b322
desa-thomas <desa2thomas@gmail.com>
2024-08-22 16:24:58 -0400

    created new file IGposting.py
    File contains functions for making requests to the instagram Graph API.
    There is currently a bug with the instagram graph API which does not allow post requests to their publish media endpoint.
    https://developers.facebook.com/community/threads/1819400801919840/

---
+++ IGposting.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from config import user_id, user_key, video_link
+
+def create_container(): 
+    caption = "beta testing..."
+    container_request = requests.post(url = f"https://graph.instagram.com/v20.0/{user_id}/media",
+                      params={
+                          "media_type" : "REELS",
+                          "video_url" : video_link,
+                          "caption" : caption, 
+                          "share_to_feed": True,
+                          "access_token": user_key
+                      })
+    print(container_request.status_code)
+    print(container_request.content)
+
+    #container successfully created
+    if(container_request.status_code == 200): 
+        containerID = json.loads(container_request.content.decode('utf-8'))["id"]
+
+        print(containerID)
+        #check status of container upload
+        r = requests.get(url=f"https://graph.instagram.com/{containerID}?fields=status_code",
+                         params={
+                                 "access_token": user_key})
+        
+        print(r.status_code)
+        print(r.content)
+
+    return
+
+create_container()
